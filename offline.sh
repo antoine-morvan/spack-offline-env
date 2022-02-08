c286bdb89128073784f4e9aef4012b26d6af5643
Antoine Morvan <antoine.morvan.pro@gmail.com>
2022-02-08 01:11:58 -0800

    Fix tmpdir

--- offline.sh
+++ offline.sh
@@ -9,6 +9,10 @@ DIR=$(cd $(dirname $0) && pwd)
 export SPACK_USER_CACHE_PATH="${DIR}/spack_user_cache"
 MIRROR_NAME=offline_spack_mirror
 
+TMP="${DIR}/tmp"
+TMPDIR="${TMP}"
+mkdir -p tmp
+
 echo "
 ##
 ## 1. Cleanup (optional)
