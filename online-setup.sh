8a7cfb8fd505154b8653135313aa89b544224d01
Antoine Morvan <antoine.morvan.pro@gmail.com>
2022-11-17 13:13:22 +0100

    Fix online script

--- online-setup.sh
+++ online-setup.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 set -eu
 
-echo "ERROR: todo"
-exit 1
-
-DIR=${DIR:-$(dirname $(readlink -f $0))}
+SCRIPT_DIR=${SCRIPT_DIR:-$(dirname $(readlink -f $0))}
+TMPDIR=${TMPDIR:-"${SCRIPT_DIR}/.tmp"}
 
 CLEAN=NO
-SPACK_ENV_DIR=${DIR}/simple_env
+SPACK_ENV_DIR=${SCRIPT_DIR}/simple_env
 
 ##
 ## Read arguments
@@ -30,13 +28,13 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-SPACK_ROOT="${DIR}/git/spack/"
-SPACK_BOOTSTRAP_ROOT="${DIR}/spack_bootstrap"
-SPACK_USER_CACHE_PATH="${DIR}/spack_user_cache"
-SPACK_MIRROR_PATH="${DIR}/spack_mirror"
+SPACK_GIT_ROOT="${SPACK_ENV_DIR}/spack_git_root/"
+SPACK_BOOTSTRAP_ROOT="${SPACK_ENV_DIR}/spack_bootstrap_root"
+SPACK_USER_CACHE_PATH="${SPACK_ENV_DIR}/spack_user_cache"
+SPACK_MIRROR_PATH="${SPACK_ENV_DIR}/spack_mirror"
 MIRROR_NAME=offline_spack_mirror
-TMP="${DIR}/.tmp"
-TMPDIR="${TMP}"
+
+TMP="${TMPDIR}"
 
 export SPACK_USER_CACHE_PATH
 export TMP
@@ -58,33 +56,22 @@ echo "
 ## 2. Load & Init Spack
 ##"
 source "${DIR}/git/spack/share/spack/setup-env.sh"
-# /!\ Disable github action to force clingo to be built from sources
-# This makes the bootstrap longer, but the mirror needs it to be sound
-spack bootstrap untrust github-actions
-spack bootstrap root "${SPACK_BOOTSTRAP_ROOT}"
-spack config add config:source_cache:"${SPACK_MIRROR_PATH}"
-spack compiler find
+
+function configureEnv() {
+    spack bootstrap root "${SPACK_BOOTSTRAP_ROOT}"
+    spack config add config:source_cache:"${SPACK_MIRROR_PATH}"
+}
+configureEnv
 
 echo "
 ##
-## 3. Init offline mirror
-##"
-set +e
-RES=$(spack mirror list | grep "${MIRROR_NAME}" | wc -l)
-set -e
-if [ $RES == 0 ]; then
-    spack mirror add "${MIRROR_NAME}" "file://${SPACK_MIRROR_PATH}"
-fi
-
-echo "
-##
-## 4. Bootstrap Clingo
+## 3. Bootstrap Clingo
 ##"
 spack -d spec zlib
 
 echo "
 ##
-## 5. Init offline env
+## 4. Init offline env
 ##"
 spack env activate -d "${DIR}/"
 spack concretize -f
